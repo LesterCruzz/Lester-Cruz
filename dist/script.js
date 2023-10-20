577343f91083c30a5637bb9ecf0bee0f867f0a0c
Lester <cpe.cruz.marklester@gmial.com>
2023-10-20 19:08:00 +0800

    test

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,5 @@
 const sideHeader = document.querySelector(".sideHeader");
-const red = document.querySelector(".sideHeader #service");
+
 
 window.addEventListener("scroll", () => {
     console.log(window.scrollY);
@@ -7,11 +7,6 @@ window.addEventListener("scroll", () => {
         sideHeader.classList.add("open");
     } else {
         sideHeader.classList.remove("open");
-    }
-    if (window.scrollY >= 300) {
-        red.classList.add("open");
-    } else {
-        red.classList.remove("open");
     }
 });
 
